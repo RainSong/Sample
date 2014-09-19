2397ee19d9cbe8629952cf660af137b3f2c3d243
RainSong <714224605@qq.com>
2014-09-19 13:15:02 +0800

    edit

--- DataTableSample/Program.cs
+++ DataTableSample/Program.cs
@@ -15,25 +15,59 @@ namespace DataTableSample
             //var value = table.Compute("carnum-onlinenum", string.Empty);
             var value = table.Compute("1+2", "");
             Console.WriteLine(value);
+
+            value = Comput();
+
             Console.ReadKey();
         }
 
         private static DataTable BuildTable()
         {
             var dt = new DataTable();
-            //var dc = new DataColumn("CarNum", typeof(double));
-            //dt.Columns.Add(dc);
-            //dc = new DataColumn("OnlineNum", typeof(double));
-            //dt.Columns.Add(dc);
-            //var dr = dt.NewRow();
-            //dr["CarNum"] = 500;
-            //dr["OnlineNum"] = 300;
-            //dt.Rows.Add(dr);
-            //dr = dt.NewRow();
-            //dr["CarNum"] = 400;
-            //dr["OnlineNum"] = 200;
-            //dt.Rows.Add(dr);
+            var dc = new DataColumn("CarNum", typeof(double));
+            dt.Columns.Add(dc);
+            dc = new DataColumn("OnlineNum", typeof(double));
+            dt.Columns.Add(dc);
+            var dr = dt.NewRow();
+            dr["CarNum"] = 500;
+            dr["OnlineNum"] = 300;
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["CarNum"] = 400;
+            dr["OnlineNum"] = 200;
+            dt.Rows.Add(dr);
             return dt;
+        }
+
+        private static object Comput()
+        {
+            var table = new DataTable();
+
+            var colId = new DataColumn("id", typeof(System.String));
+            table.Columns.Add(colId);
+
+            var colPrice = new DataColumn("price", typeof(System.Decimal));
+            table.Columns.Add(colPrice);
+
+            var colQuantity = new DataColumn("quantity", typeof(System.Int32));
+            table.Columns.Add(colQuantity);
+
+            var tr = table.NewRow();
+
+            tr["id"] = "n001";
+            tr["price"] = 1.5M;
+            tr["quantity"] = 2;
+            table.Rows.Add(tr);
+            Object value = null;
+            try
+            {
+                value = table.Compute("Max(price)", "id='n001'");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return value;
         }
     }
 }
---
+++ JQueryEasyUI/ControlTest.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ControlTest.aspx.cs" Inherits="JQueryEasyUI.ControlTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <script type="text/javascript" src="Scripts/jquery-1.9.1.js"></script>
+    <script type="text/javascript" src="Scripts/jquery.easyui-1.3.6.js"></script>
+    <script type="text/javascript" src="Scripts/common.js"></script>
+    <link rel="stylesheet" type="text/css" href="Content/themes/default/easyui.css" />
+</head>
+<body>
+    <label for="text1">多选下拉框</label><input type="text" id="text1" />
+    <label for="text2">下拉树</label><input type="text" id="text2" />
+</body>
+<script type="text/javascript">
+    function initMultiableSelectCombobox() {
+        var options = {
+            valueField: 'id',
+            textField: 'name',
+            editable: false,
+            multiple: true,
+            width: 150,
+            panelHeight: 100,
+            data: [{ id: 0, name: 'item1' }, { id: 1, name: 'item2' }, { id: 2, name: 'item3' }],
+            onHidePanel: function () {
+                var text = $('#text1').combobox('getText');
+                if (!text || text.length == 0) text = 'do not select any one';
+                $.messager.alert('Message', text);
+            }
+        };
+        $('#text1').combobox(options);
+    }
+    function initComboboxTree() { }
+    $(document).ready(function () {
+        initMultiableSelectCombobox();
+        initComboboxTree();
+    });
+</script>
+</html>
