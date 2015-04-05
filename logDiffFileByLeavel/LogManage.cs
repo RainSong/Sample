1ccebd117e79fcfe2345dd76ccbf823cbc58ca79
714224605@qq.com <714224605@qq.com>
2015-04-05 20:51:27 +0800

    添加log4net根据级别分别记录

---
+++ logDiffFileByLeavel/LogManage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using System.IO;
+
+namespace logDiffFileByLeavel
+{
+    public class LogManage
+    {
+        public static ILog logger;
+        private static object _lock = new object();
+
+        private LogManage()
+        {
+            
+        }
+
+        public static ILog GetLogger()
+       {
+               if(logger==null)
+               {
+                      lock(_lock)
+                      {
+                             if(logger==null)
+                             {
+                                 var fi = new FileInfo(@"D:\workspace\Sample\logDiffFileByLeavel\App.config");
+                                 log4net.Config.XmlConfigurator.Configure(fi);
+                                 logger = log4net.LogManager.GetLogger("logger");
+                             }
+                      }
+               }
+               return logger;
+       }
+    }
+}
---
+++ logDiffFileByLeavel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logDiffFileByLeavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var log = LogManage.GetLogger();
+                log.Info("开始记录日志");
+                var ex = new Exception("这是一个测试用的异常");
+                log.Error("这是一个测试异常", ex);
+                log.Info("记录完成");
+                Console.ReadKey();
+            }
+            catch(Exception ex) 
+            {
+                var msg = ex.Message;
+            }
+        }
+    }
+}
