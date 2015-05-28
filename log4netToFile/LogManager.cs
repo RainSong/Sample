5de784b4a7642a07d6b504cdeb44ecef4b91bd6e
RainSong <714224605@qq.com>
2015-05-28 08:22:57 +0800

    增加log4net按日期生成日志文件

---
+++ log4netToFile/LogManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.IO;
+using System.Text;
+using System.Web;
+
+namespace log4netToFile
+{
+    /// <summary>
+    /// LogManager 的摘要说明
+    /// </summary>
+    public class LogManager
+    {
+        private static log4net.ILog logger;
+        private static object _lock = new object();
+        private LogManager()
+        {
+            //
+            // TODO: 在此处添加构造函数逻辑
+            //
+        }
+        public static log4net.ILog GetLogger()
+        {
+            if (logger == null)
+            {
+                lock (_lock)
+                {
+                    if (logger == null)
+                    {
+                        string path = "D:\\workspace\\git\\Sample\\log4netToFile\\bin\\Debug\\log4netConfig.xml";
+                        FileInfo fi = new FileInfo(path);
+                        log4net.Config.XmlConfigurator.Configure(fi);
+                        logger = log4net.LogManager.GetLogger("logger");
+                    }
+                }
+            }
+            return logger;
+        }
+
+
+        public static void LogError(Exception ex)
+        {
+            log4net.ILog logger = LogManager.GetLogger();
+            logger.Error(ex);
+        }
+
+        public static void LogInfo(object msg)
+        {
+            log4net.ILog logger = LogManager.GetLogger();
+            logger.Info(msg);
+        }
+
+        public static void LogError(string messsage, Exception ex)
+        {
+            log4net.ILog logger = LogManager.GetLogger();
+            logger.Error(new Exception(messsage, ex));
+        }
+    }
+}
---
+++ log4netToFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4netToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action a = () =>
+            {
+                while (true)
+                {
+                    LogManager.LogError(new Exception("这是测试异常"));
+                    Console.WriteLine("写入一条异常日志");
+                }
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                Task t = new Task(a);
+                t.Start();
+            }
+            Console.ReadKey();
+        }
+    }
+}
