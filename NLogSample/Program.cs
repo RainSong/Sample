38aaaa4d94ffe3faf0a0d3d4d4479b0878c4fa88
714224605@qq.com <714224605@qq.com>
2018-01-23 11:11:11 +0800

    添加NLOG的例子 Signed-off-by: 714224605@qq.com <714224605@qq.com>

---
+++ NLogSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elogger = NLog.LogManager.GetLogger("ErrorToFileLogger");
+            var ilogger = NLog.LogManager.GetLogger("InfoToFileLogger");
+            elogger.Info("记录一条日志信息");
+            try
+            {
+                Fun();
+            }
+            catch (Exception ex)
+            {
+                elogger.Error(ex, "捕获异常");
+            }
+        }
+
+        static void Fun()
+        {
+            string a = null;
+            a.Equals("abc");
+        }
+    }
+}
